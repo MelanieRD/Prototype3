332b98ec2eb3040ca8261f3c47c59f2855db2459
Melanie Meléndez <57066220+MelanieRD@users.noreply.github.com>
2021-12-20 13:11:54 -0400

    Add Prueba
    Eliminé una linea del código comentada para probar el controlador de versiones Git.

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -10,7 +10,7 @@ public class PlayerController : MonoBehaviour
     public GameObject playerObj;
 
 
-    // Start is called before the first frame update
+   
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
