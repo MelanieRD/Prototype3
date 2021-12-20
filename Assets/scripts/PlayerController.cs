6da32fd6e3cca2786d8cbf8399c58f791a916399
Melanie Meléndez <57066220+MelanieRD@users.noreply.github.com>
2021-12-20 13:51:29 -0400

    improving jump movement

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float acceleration = 10f;
+    
     public float speed = 10f;
     public GameObject playerObj;
+    public float acceleration = 10f;
+    public float gravityModifier;
 
+    private bool onGround = true;
 
    
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
@@ -23,13 +27,19 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
         {
             playerObj.transform.position = Vector3.right * Time.deltaTime* speed;
-                    }
+        }
         
 
         // Saltar
-            if (Input.GetKeyDown(KeyCode.Space)) {
+            if (Input.GetKeyDown(KeyCode.Space) && onGround == true) {
 
             playerRb.AddForce(Vector3.up * acceleration, ForceMode.Impulse);
+            onGround = false;
         } 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onGround = true;
     }
 }
